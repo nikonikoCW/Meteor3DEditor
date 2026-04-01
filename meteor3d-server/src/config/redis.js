3873c7c54ced87bc4604e69def1b45735d9e9e9a
你的猫掉了耶 <84675738@qq.com>
2026-04-01 14:46:56 +0800

    fix：配置文件

--- meteor3d-server/src/config/redis.js
+++ meteor3d-server/src/config/redis.js
@@ -2,8 +2,10 @@
  * Redis 连接配置
  * 用于 Bull 任务队列
  */
+require('dotenv').config();
+
 module.exports = {
-    host: '192.168.102.200',
-    port: 6779,
-    password: 'chenwei'
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD || 'chenwei'
 };
